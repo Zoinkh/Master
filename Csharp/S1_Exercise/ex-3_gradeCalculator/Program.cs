9f0265c6be5f6b4e39b43a5115684920b865c3d6
Zoinkh <zoinkberg@hotmail.com>
2025-03-03 00:55:42 +0700

    Update

---
+++ Csharp/S1_Exercise/ex-3_gradeCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Numerics;
+
+namespace ex_3_gradeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tGrade Calculator");
+            Console.Write("OOP :");
+            double oop = double.Parse(Console.ReadLine());
+            Console.Write("Satistic :");
+            double sat = double.Parse(Console.ReadLine());
+            Console.Write("English :");
+            double eng = double.Parse(Console.ReadLine());
+            Console.Write("Writing Skill :");
+            double writ = double.Parse(Console.ReadLine());
+            Console.Write("Microprocessor :");
+            double micro = double.Parse(Console.ReadLine());
+            Console.Write("DataBase :");
+            double db = double.Parse(Console.ReadLine());
+            Console.Write("Python :");
+            double py = double.Parse(Console.ReadLine());
+            double total = oop + sat + eng + writ + micro + db + py;
+            double avg = total / 7;
+            Console.WriteLine("Average :" + avg);
+            Console.WriteLine("Total :" + total);
+            if (avg > 90)
+            {
+                Console.WriteLine("Your grade is A");
+            }
+            else if (avg > 80)
+            {
+                Console.WriteLine("Your grade is B");
+            }
+            else if (avg > 70)
+            {
+                Console.WriteLine("Your grade is C");
+            }
+            else if (avg > 60)
+            {
+                Console.WriteLine("Your grade is D");
+            }
+            else if (avg > 50)
+            {
+                Console.WriteLine("Your grade is E");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is F");
+            }
+        }
+    }
+}
---
+++ Csharp/S1_Exercise/ex-9_Hote_lSystem/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_9_Hote_lSystem
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public Person() { }
+
+        public Person(string Name, string Address, string Phone)
+        {
+            this.Name = Name;
+            this.Address = Address;
+            this.Phone = Phone;
+        }
+
+        protected void DisplayPersonProfile()
+        {
+            string info = $"Name: {Name}, Address: {Address}, Phone: {Phone}";
+            Console.Write(info);
+        }
+    }
+}
